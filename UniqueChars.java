4e3848ac6aff24ac74692b5fbafacb56a9ce7055
EdanZiv99 <ziv.edan@post.runi.ac.il>
2024-02-11 12:47:26 +0200

    second_try

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,20 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String ans = "" + (char) (s.charAt(0));
+        int i = 1; 
+        while (i < s.length()) {
+            char ch = s.charAt(i); 
+            if (ch == ' ') {
+                ans = ans + ch; 
+            }
+            if (ans.indexOf(ch) == -1) {
+                ans = ans + ch; 
+                i++;
+            }  else  { 
+                i++;
+            }
+        }
+        return ans;
     }
 }
