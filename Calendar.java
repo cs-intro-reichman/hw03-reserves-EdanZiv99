43d89d75fc2b486cf047388a80e8701eda65f7fe
EdanZiv99 <ziv.edan@post.runi.ac.il>
2024-02-13 17:37:51 +0200

    11th_try

--- Calendar.java
+++ Calendar.java
@@ -1,13 +1,14 @@
 /** 
  * Prints the calendars of all the years in the 20th century.
  */
-public class Calendar1 {	
+public class Calendar {	
     // Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
 	static int year = 1900;
 	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
+    
 	/** 
 	 * Prints the calendars of all the years in the 20th century. Also prints the  
 	 * number of Sundays that occured on the first day of the month during this period.
@@ -16,24 +17,25 @@ public class Calendar1 {
 		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
 	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
 	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
-	    int debugDaysCounter = 0; 
+	   
+        //int debugDaysCounter = 0; 
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
-		int countSundays = 0;
-		while (year <= 1999) {
-			if (dayOfWeek == 1) {
-				System.out.print(dayOfMonth + "/" + month + "/" + year);
-				System.out.println(" Sunday");
-			} else {
-				System.out.println(dayOfMonth + "/" + month + "/" + year);
-			}
-			advance(); 
-			
-			if (dayOfMonth == 1 && dayOfWeek == 1) {
-				countSundays++;		
+        int givenYear = Integer.parseInt(args[0]);  
+
+		while (year < givenYear) {
+			advance();
+
+		while (year == givenYear) {
+            if (dayOfWeek == 1) {
+                System.out.print(dayOfMonth + "/" + month + "/" + year);
+                System.out.println(" Sunday");
+            } else {
+                System.out.println(dayOfMonth + "/" + month + "/" + year);
+            }
+                advance(); 	
 			}
 		}
-		System.out.println("During the 20th century, " + countSundays + " Sundays fell on the first day of the month");
 	}
 	 		//debugDaysCounter++;
 	 		//// If you want to stop the loop after n days, replace the condition of the
